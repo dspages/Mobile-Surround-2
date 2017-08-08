7eda692d44a24aa3fbc139878f5507a3017d70cf
Nicholas Bacon <Mordecailloyd@gmail.com>
2017-08-08 09:50:09 -0700

    frontend and rails views

---
+++ frontend/components/file_upload/upload.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class Upload extends React.Component {
+  render () {
+
+
+
+    return (
+    <form>
+      <input type = 'data' >
+
+      </input>
+    </form>
+
+  );
+  }
+}
